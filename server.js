c5d293ed1faa2d2d9b7a4aa412e5fbbb7129ba6f
dahalh <dahalh@outlook.com>
2022-07-26 15:01:38 +1000

    initialization

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+const app = express();
+
+const PORT = process.env.port || 8000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+// mongodb connect
+// import { dbConnect } from "./src/config/dbConfig.js";
+// dbConnect();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "you have reached the client api",
+  });
+});
+
+// error handling
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  res.status(err.status || 500);
+  res.json({
+    status: "error",
+    message: err.message,
+  });
+});
+
+// bound app with port
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`server is running on http://localhost:${PORT}`);
+});
---
+++ src/config/dbConfig.js
@@ -0,0 +1,11 @@
+// import mongoose from "mongoose";
+
+// export const dbConnect = () => {
+//   try {
+//     const conn = mongoose.connect(process.env.MONGO_CLIENT);
+
+//     conn && console.log("mongoDB is connected");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
